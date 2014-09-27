f1bf98f9714d9fb82b1da0405a862a6291855f99
Andy <andrew.trinker@gmail.com>
2014-09-26 20:18:59 -0400

    Added x,y location ints, added Skin enum.

--- The Nut Job/Ball.cs
+++ The Nut Job/Ball.cs
@@ -8,10 +8,27 @@ namespace The_Nut_Job
     class Ball
     {
         int Radius { get; private set; }
-        
+        Skin BallSkin { get; private set; }
+        int XCenter { get; private set; }
+        int YCenter { get; private set; }
+
         public Ball()
         {
+            Radius = 50; //pixels.
+            XCenter = 0;
+            YCenter = 0;
+            BallSkin = Skin.Default;
+        }
 
+        public void SetLocation(int x, int y)
+        {
+            XCenter = x;
+            YCenter = y;
+        }
+
+        public void SetSkin(Skin skin)
+        {
+            BallSkin = skin;
         }
     }
 }
