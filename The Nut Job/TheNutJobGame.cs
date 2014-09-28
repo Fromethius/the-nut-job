745975f20fc6b16115086bb08fb3c95cd17bacad
Fromethius <xfromethius@gmail.com>
2014-09-28 01:56:09 -0400

    My end

--- The Nut Job/TheNutJobGame.cs
+++ The Nut Job/TheNutJobGame.cs
@@ -45,7 +45,6 @@ namespace The_Nut_Job
         protected override void Update(GameTime gameTime)
         {
             Input.Update();
-            ball.Update(gameTime);
 
             if (Input.IsMouseLeftClick())
             {
@@ -64,22 +63,40 @@ namespace The_Nut_Job
                 float xOffset = radius * (float)Math.Cos(degrees);
                 float yOffset = radius * (float)Math.Sin(degrees);
 
-                Vector2 circumferencePosition = new Vector2((int)(origin.X + xOffset), (int)(origin.Y + yOffset));
+                Vector2 circumferencePosition = new Vector2(origin.X + xOffset, origin.Y + yOffset);
 
                 foreach (var path in sketcher.lines)
                 {
-                    if (path.Contains(circumferencePosition))
+                    for (int i = 1; i < path.Count - 1; i++)
                     {
-                        int collisionIndex = path.IndexOf(circumferencePosition);
-                        Vector2 previousPoint = path[collisionIndex - 1];
-                        Vector2 nextPoint = path[collisionIndex + 1];
+                        LinePoint point = path[i];
 
-                        Vector2 connecting = nextPoint - previousPoint;
-                        Vector2 normal = new Vector2();
-                        ball.Bounce();
+                        Vector2 p = origin;
+                        Vector2 p2 = circumferencePosition * 1.1f;
+                        Vector2 q = point.Previous.Position;
+                        Vector2 q2 = point.Next.Position;
+
+                        Vector2? intersectionPoint = GetLineIntersectionPoint(p, p2, q, q2);
+
+                        // bool intersectionHappened = Intersection(q2 - q, p, ball.Image.Width / 2);
+
+                        if (intersectionPoint.HasValue)
+                        {
+                            p2 = intersectionPoint.Value;
+
+                            Vector2 directionP = p2 - p;
+                            Vector2 directionQ = q2 - q;
+
+                            Vector2 u = (Vector2.Dot(directionP, directionQ) / Vector2.Dot(directionQ, directionQ)) * directionQ;
+                            Vector2 w = directionP - u;
+
+                            Vector2 newVelocity = w - u;
+
+                            ball.SetVelocity(newVelocity);
+                        }
                     }
 
-
+                    // New vector == -2 * (V dot N) * N + V
                 }
             }
 
@@ -89,7 +106,44 @@ namespace The_Nut_Job
                 ball.SetVelocity(new Vector2(0, 10));
             }
 
+            ball.Update(gameTime);
             base.Update(gameTime);
+        }
+
+        public bool Intersection(Vector2 d, Vector2 f, float r)
+        {
+            float a = Vector2.Dot(d, d);
+            float b = 2 * Vector2.Dot(f, d);
+            float c = Vector2.Dot(f, f) - r * r;
+
+            float discriminant = b * b - 4 * a * c;
+            if (discriminant >= 0)
+            {
+                discriminant = (float)Math.Sqrt(discriminant);
+
+                float t1 = (-b - discriminant) / (2 * a);
+                float t2 = (-b + discriminant) / (2 * a);
+
+                return (t1 >= 0 && t1 <= 1) || (t2 >= 0 && t2 <= 1);
+            }
+
+            return false;
+        }
+
+        Vector2? GetLineIntersectionPoint(Vector2 p0, Vector2 p1, Vector2 p2, Vector2 p3)
+        {
+            Vector2 s1 = new Vector2(p1.X - p0.X, p1.Y - p0.Y);
+            Vector2 s2 = new Vector2(p3.X - p2.X, p3.Y - p2.Y);
+
+            float s = (-s1.Y * (p0.X - p2.X) + s1.X * (p0.Y - p2.Y)) / (-s2.X * s1.Y + s1.X * s2.Y);
+            float t = (s2.X * (p0.Y - p2.Y) - s2.Y * (p0.X - p2.X)) / (-s2.X * s1.Y + s1.X * s2.Y);
+
+            if (s >= 0 && s <= 1 && t >= 0 && t <= 1)
+            {
+                return new Vector2(p0.X + (t * s1.X), p0.Y + (t * s1.Y));
+            }
+
+            return null; // No collision
         }
 
         protected override void Draw(GameTime gameTime)
